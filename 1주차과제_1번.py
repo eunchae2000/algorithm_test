eab0f0c714a4bec28e53ca591a87d7ec9e5b90da
coco <tldjs3651@naver.com>
2022-01-06 15:38:24 +0900

    하나의 숫자를 내림차순으로 정렬하여 출력

---
+++ 1주차과제_1번.py
@@ -0,0 +1,12 @@
+# 수가 주어지면 각 자리의 수를 내림차순으로 정렬해보자.
+
+# 수를 입력
+n = int(input())
+# 입력받은 수를 int형을 str형으로 바꿔주고 리스트로 저장
+num = list(map(int, str(n)))
+# 해당 리스트를 내림차순으로 정렬해줌
+num.sort(reverse=True)
+
+# 정렬한 리스트를 하나씩 붙여서 출력
+for i in num:
+    print(i, end="")
--- 정렬_1번.py
+++ 정렬_1번.py
@@ -6,3 +6,18 @@ for i in range(1, n+1):
     for j in range(i):
         print("*", end="")
     print()
+
+print("-===============-")
+
+for i in range(1, n+1):
+    for _ in range(i):
+        print("*", end="")
+    for _ in range(n-i):
+        print(" ", end="")
+    print()
+
+print("-===============-")
+
+for i in range(n+1, 1):
+    for j in i:
+        print("*", end="")
