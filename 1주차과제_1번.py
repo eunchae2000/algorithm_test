fd4689e3d250e9f0f662fb9a3d7d2eb2b1ad3c7a
coco <tldjs3651@naver.com>
2022-01-06 15:46:14 +0900

    알고리즘 공부

--- 1주차과제_1번.py
+++ 1주차과제_1번.py
@@ -1,9 +1,9 @@
 # 수가 주어지면 각 자리의 수를 내림차순으로 정렬해보자.
 
 # 수를 입력
-n = int(input())
+N = int(input())
 # 입력받은 수를 int형을 str형으로 바꿔주고 리스트로 저장
-num = list(map(int, str(n)))
+num = list(map(int, str(N)))
 # 해당 리스트를 내림차순으로 정렬해줌
 num.sort(reverse=True)
 
