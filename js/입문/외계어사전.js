b02833d7e7173268c345396028684454af753a81
coco <tldjs3651@naver.com>
2023-02-24 14:32:23 +0900

    파일이동

--- js/입문/외계어사전.js
+++ js/입문/외계어사전.js
@@ -1,4 +1,7 @@
 function solution(spell, dic) {
     spell = spell.sort().join("");
-    return dic.map(a => a.split("").sort().join("")).find(a => a === spell) === undefined ? 2 : 1
+    console.log(spell)
+    return dic.map(a => a.split().sort().join("")).find(a => a === spell) === undefined ? 2 : 1
 }
+
+console.log(solution(["p", "o", "s"], ["sod", "eocd", "qixm", "adio", "soo"]))
