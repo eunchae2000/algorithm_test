919a07bd83c16024285aa8bfdc9c7ace3d1e069a
coco <tldjs3651@naver.com>
2023-04-07 23:01:08 +0900

    DFS

---
+++ study/DFS,BFS/바이러스.py
@@ -0,0 +1,20 @@
+from collections import deque
+
+n = int(input())
+m = int(input())
+network = [[] for _ in range(n+1)]
+visited = [0] * (n+1)
+answer = [1]
+for _ in range(m):
+    a, b = map(int, input().split())
+    network[a] += [b]
+    network[b] += [a]
+
+# def dfs(m):
+#     visited[m] = 1
+#     for i in network[m]:
+#         if visited[i] == 0:
+#             dfs(i)
+# dfs(1)
+
+print(sum(visited)-1)
