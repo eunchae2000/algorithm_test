a4b95b8efeb873e5f379cc5c0c28b247f6993fe3
coco <tldjs3651@naver.com>
2024-01-02 15:59:10 +0900

    DFS, BFS 기초 알고리즘

---
+++ study/DFS,BFS/알고리즘수업1_깊이우선탐색.py
@@ -0,0 +1,27 @@
+import sys
+sys.setrecursionlimit(10**9)
+input = sys.stdin.readline
+
+n, m, r = map(int, input().split())
+graph = [[] for _ in range(n+1)]
+visited = [0]*(n+1)
+
+answer = 1
+def dfs(graph, v, visited):
+    global answer
+    visited[v] = answer
+    graph[v].sort()
+    
+    for i in graph[v]:
+        if visited[i] == 0:
+            answer += 1
+            dfs(graph, i, visited)
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+dfs(graph, r, visited)
+for i in range(1, n+1):
+    print(visited[i])
---
+++ study/DFS,BFS/알고리즘수업4_너비우선탐색.py
@@ -0,0 +1,36 @@
+import sys
+from collections import deque
+sys.setrecursionlimit(10**9)
+input = sys.stdin.readline
+
+n, m, r = map(int, input().split())
+graph = [[] for _ in range(n+1)]
+visited = [0]*(n+1)
+
+answer = 1
+def bfs(graph, v, visited):
+    global answer
+    queue = deque([v])
+    visited[v] = answer
+    
+    while queue:
+        q = queue.popleft()
+        for i in graph[q]:
+            if visited[i] == 0:
+                queue.append(i)
+                answer += 1
+                visited[i] = answer
+    
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+for i in range(n+1):
+    graph[i].sort(reverse=True)
+
+bfs(graph, r, visited)
+
+for i in range(1, n+1):
+    print(visited[i])
+    
