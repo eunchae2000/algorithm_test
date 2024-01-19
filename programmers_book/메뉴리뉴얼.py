7873cf81da24222d1b4b9d51822987a275376f23
coco <tldjs3651@naver.com>
2024-01-19 21:32:54 +0900

    해시 알고리즘

--- programmers_book/메뉴리뉴얼.py
+++ programmers_book/메뉴리뉴얼.py
@@ -10,10 +10,10 @@ def solution(orders, course):
             combi = combinations(sorted(o), c)
             menu += combi
         count = Counter(menu)
-        if (len(count) != 0 and max(count.values()) != 1):
+        if len(count) != 0 and max(count.values()) != 1:
             for m, cnt in count.items():
                 if cnt == max(count.values()):
                     answer.append(''.join(m))
     return sorted(answer)
-        
+
 print(solution(["ABCEG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"], [2, 3, 4]))
