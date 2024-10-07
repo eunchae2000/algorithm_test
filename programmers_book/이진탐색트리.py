3418f7e23deed08bee61aca8fb56f612d557adac
coco <tldjs3651@naver.com>
2024-10-07 21:30:16 +0900

    트리: 알고리즘 2문제

--- programmers_book/이진탐색트리.py
+++ programmers_book/이진탐색트리.py
@@ -3,7 +3,7 @@ class Node:
         self.left = None
         self.right = None
         self.val = key
-    
+        
 class BST:
     def __init__(self):
         self.root = None
@@ -13,7 +13,7 @@ class BST:
         else:
             current = self.root
             while True:
-                if key<current.val:
+                if key < current.val:
                     if current.left:
                         current = current.left
                     else:
@@ -25,25 +25,26 @@ class BST:
                     else:
                         current.right = Node(key)
                         break
+    
     def search(self, key):
         current = self.root
-        while current ans current.val != key:
-            if key<current.val:
+        while current and current.val != key:
+            if key < current.val:
                 current = current.left
             else:
                 current = current.right
         return current
-    
 
-def solution(lst, search_list):
+def solution(lst, search_lst):
     bst = BST()
     for key in lst:
         bst.insert(key)
-    
     result = []
-    for search_val in search_list:
+    for search_val in search_lst:
         if bst.search(search_val):
             result.append(True)
         else:
             result.append(False)
     return result
+
+print(solution([5, 3, 8, 4, 2, 1, 7, 10], [1, 2, 5, 6]))
