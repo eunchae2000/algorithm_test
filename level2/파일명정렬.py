f5e0da5c4b3570145b5b8f4b8a34349ac39f88e8
coco <tldjs3651@naver.com>
2024-11-01 17:17:38 +0900

    알고리즘: level2 '파일명정렬'

--- level2/파일명정렬.py
+++ level2/파일명정렬.py
@@ -14,11 +14,5 @@ def solution(files):
                 tail += file[i:]
                 break
         answer.append((head, number, tail))
-        
-    answer.sort(key=lambda x: (x[0].upper(), int(x[1]))) 
-    
+    answer.sort(key=lambda x:(x[0].upper, int(x[1])))
     return [''.join(temp) for temp in answer]
-
-
-print(solution(["img12.png", "img10.png", "img02.png", "img1.png", "IMG01.GIF", "img2.JPG"]))
-print(solution(["F-5 Freedom Fighter", "B-50 Superfortress", "A-10 Thunderbolt II", "F-14 Tomcat"]))
